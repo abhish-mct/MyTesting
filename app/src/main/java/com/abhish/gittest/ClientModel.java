16a62990189f6373249487439a28708361d7541a
abhishbhawsar <abhish.bhawsar@mindcrewtech.com>
2021-10-27 15:45:43 +0530

    Client Model Class is added

---
+++ app/src/main/java/com/abhish/gittest/ClientModel.java
@@ -0,0 +1,30 @@
+package com.abhish.gittest;
+
+public class ClientModel {
+
+    String cname,cid,ctype;
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public String getCtype() {
+        return ctype;
+    }
+
+    public void setCtype(String ctype) {
+        this.ctype = ctype;
+    }
+}
