d818cacb5a46ee7d17db3fee4844a1fdd60e615d
abhishbhawsar <abhish.bhawsar@mindcrewtech.com>
2021-10-27 13:30:08 +0530

    Getter Settr Added in side ProviderModel class

--- app/src/main/java/com/abhish/gittest/ProviderModel.java
+++ app/src/main/java/com/abhish/gittest/ProviderModel.java
@@ -4,4 +4,28 @@ public class ProviderModel {
     String name;
     String providerId;
     String serviceType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
 }
